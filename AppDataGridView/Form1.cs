5182a41aeef5761f58fed2b932f22226a0777fdf
Juan Leonel <juanl.albores.c@gmail.com>
2018-12-03 12:10:39 -0500

    Cambios en los metodos

--- AppDataGridView/Form1.cs
+++ AppDataGridView/Form1.cs
@@ -31,6 +31,7 @@ namespace AppDataGridView
             tabla.Columns.Add("Cant_Abierto", typeof(int));
             tabla.Columns.Add("Abierto", typeof(bool));
 
+            //tabla.Columns.Add("Folios", typeof(string));
             // Here we add five DataRows.
             //             ID Producto Inventario  Cant_Consumido   Consumido  Cant_Abierto     Abierto
             tabla.Rows.Add(1, "Cola", 0, 0, 1, 0, 0);
@@ -55,30 +56,43 @@ namespace AppDataGridView
                         Producto = pro.FirstOrDefault().Field<string>("Producto"),
                         Inventario = pro.Count(),
                         Cant_Consumido = pro.Count(x => x.Field<bool>("Consumido") == true),
-                        Cant_Abierto = pro.Count(x => x.Field<bool>("Abierto") == true)
+                        Cant_Abierto = pro.Count(x => x.Field<bool>("Abierto") == true),
+                        //Folios  = pro.FirstOrDefault().Field<String>("Folios")
                     };
 
 
             DataTable table = new DataTable();
             table = Utilities.ToDataTable(t.ToList());
-
-
-
+            
             tabla.Merge(table, false, MissingSchemaAction.Add);
-
-            DataView dv = tabla.DefaultView;
-            dv.Sort = "ID ASC, Inventario DESC";
-
-            tabla = dv.ToTable();
-
             
+            tabla = orderByTable("ID ASC, Inventario DESC");
 
-            
             dataGridView1.DataSource = tabla;
         }
 
+        private DataTable orderByTable(String criterio) {
+            DataView dv = tabla.DefaultView;
+            dv.Sort = criterio;
+            return dv.ToTable();
+        }
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(238, 239, 249);
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dataGridView1.DefaultCellStyle.SelectionBackColor = Color.FromArgb(23, 101, 201);//Color.DarkTurquoise;
+            dataGridView1.DefaultCellStyle.SelectionForeColor = Color.WhiteSmoke;
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(20, 25, 72);
+            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dataGridView1.AutoResizeColumns();
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
             cargaTabla();
             ocultar();
         }
@@ -88,17 +102,18 @@ namespace AppDataGridView
             for (int row = 0; row < dataGridView1.Rows.Count; row++)
             {
                 
-                if (dataGridView1.Rows[row].Cells[7].Value != null && !dataGridView1.Rows[row].Cells[7].Value.Equals("+"))
+                if (dataGridView1.Rows[row].Cells["Accion"].Value != null && !dataGridView1.Rows[row].Cells["Accion"].Value.Equals("+"))
                 {
                     dataGridView1.CurrentCell = null;
                     dataGridView1.Rows[row].Visible = false;
 
-                    var cell = ((DataGridViewButtonCell)dataGridView1.Rows[row].Cells[7]);
+                    var cell = ((DataGridViewButtonCell)dataGridView1.Rows[row].Cells["Accion"]);
                      
                     cell.FlatStyle = FlatStyle.Flat;
                     cell.Style.ForeColor = Color.DarkGray;
                     cell.ReadOnly = true;
 
+                   
                     //DataGridViewCell cell_ = dataGridView1.Rows[row].Cells[7];
                     //DataGridViewCheckBoxCell chkCell = cell_ as DataGridViewCheckBoxCell;
                     //chkCell.Value = false;
@@ -116,9 +131,9 @@ namespace AppDataGridView
 
 
                 }
-                else if (dataGridView1.Rows[row].Cells[7].Value != null && dataGridView1.Rows[row].Cells[7].Value.Equals("+")) {
+                else if (dataGridView1.Rows[row].Cells["Accion"].Value != null && dataGridView1.Rows[row].Cells["Accion"].Value.Equals("+")) {
                     dataGridView1.Rows[row].DefaultCellStyle.BackColor = Color.Gray;
-                    dataGridView1.Rows[row].Cells[7].Style.BackColor = Color.DarkGray;
+                    dataGridView1.Rows[row].Cells["Accion"].Style.BackColor = Color.DarkGray;
 
                   
 
